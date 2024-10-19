a3b52b29de1459c57e2af4584bbee8ad32939eb5
zlatakjshkarova <147971125+zlatakjshkarova@users.noreply.github.com>
2024-10-19 14:56:27 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+if (number === 2 || number === 3) {
+    alert('Вы ввели число 2, а может и 3');
+}
+
+let number1 = 2;
+let number2 = 3
+switch (nuber) {
+    case number:
+        alert('Вы ввели число 2')
+        break;
+    case number1:
+        alert(' а может и 3')
+        break;
+    default: alert("yututyuty")
+}
+
+
